c61c5ecdb20239c6c959dae2757f7e50a5a919e8
isaacdes <ixdes19982@gmail.com>
2021-08-02 23:47:41 +0530

    icon set

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,20 @@ namespace Taxi_Booking_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1316, 650);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Taxi Booking System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Taxi Booking System
+Windows application size (1332, 689)
